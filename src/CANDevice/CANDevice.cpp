0e9e8c3984ba44c962f1207c8fffddf10fc0c2e4
Matthew Yu <Matthewjkyu@gmail.com>
2021-04-10 16:19:17 -0500

    Completed API of the CANDevice and updated mbed build system.

--- src/CANDevice/CANDevice.cpp
+++ src/CANDevice/CANDevice.cpp
@@ -5,9 +5,9 @@
  * Author: Matthew Yu
  * Organization: UT Solar Vehicles Team
  * Created on: September 10th, 2020
- * Last Modified: 10/07/20
+ * Last Modified: 04/10/21
  * 
- * File Discription: This file manages the CAN class, abstracting away
+ * File Description: This file manages the CAN class, abstracting away
  * implementation logic to send and receive messages via the CAN lines and
  * providing an easy interface.
  */
@@ -20,20 +20,58 @@ CANDevice::CANDevice(
     const PinName pinRx, 
     const PinName pinTx) : can(pinRx, pinTx, CAN_BUS_BAUD) 
 {
-    bufferLock = false;
+    mailboxLock = false;
     getIdx = 0;
     putIdx = 0;
+
+    /* Initialize the default invalid CAN ID to the whitelist. */
+    whitelistLen = CAN_ID_WHITELIST_LENGTH;
+    for (uint32_t i = 0; i < whitelistLen; i++) {
+        whitelist[i] = INVALID_CAN_ID_DEFAULT;
+    }
 }
 
-char* CANDevice::getMessage() {
-    if (bufferLock) return new char[1]();
+result_t CANDevice::addFilter(uint32_t *ids, uint32_t len) {
+    if (len > CAN_ID_WHITELIST_LENGTH) return ERROR;
 
+    whitelistLen = len;
+
+    /* Cycle through the list and update the IDs. */
+    for (uint32_t i = 0; i < len; i++) {
+        whitelist[i] = ids[i];
+    }
+    return OK;
+}
+
+result_t CANDevice::sendMessage(uint32_t id, char *message, uint8_t len) {
+    return can.write(CANMessage(id, message, len));
+}
+
+result_t CANDevice::getMessage(char *message, int messageSize) {
+    /* Stall until CANDevice is ready. */
+    while (mailboxLock);
+
+    /* If there is no message, return false. */
+    if (getIdx == putIdx) return NO_MESSAGE;
+
+    mailboxLock = true;
+
+    /* Extract the first message from the mailbox. */
     CANMessage canMsg = mailbox[getIdx];
-    char * msg = new char[canMsg.len];
-    std::strcpy(msg, reinterpret_cast <char *>(canMsg.data));
 
+    /* Increment forward in the mailbox. */
     getIdx = (getIdx + 1) % CAN_BUS_SIZE;
-    return msg;
+
+    mailboxLock = false;
+
+    /* Write the message into the message parameter. */
+    // TODO: this may break during testing if messages are in reverse.
+    uint8_t length = (len < canMsg.len) ? len : canMsg.len;
+    for (uint8_t i = 0; i < length; i++) {
+        message[i] = canMsg.data[i];
+    }
+
+    return OK;
 }
 
 void CANDevice::start(const int interval) {
@@ -45,28 +83,41 @@ void CANDevice::stop() {
     tick.detach();
 }
 
+void CANDevice::clear() {
+    getIdx = 0;
+    putIdx = 0;
+    whitelistLen = 0;
+}
+
 /** Private Methods. */
 
 void CANDevice::handler() {
-    // turn LED on
-    bufferLock = true;
-    // if bus buffer is free, read a new byte
+    /* We throw out the message if we're currently locked. */
+    // TODO: in the future, we may decide that the extra couple cycles 
+    // waiting for L39 - L47 to occur is worth getting all messages.
+    if (mailboxLock) return;
+    mailboxLock = true;
+
+    /* If mailbox is not full, put a new message into the buffer. */
     if (((putIdx + 1) % CAN_BUS_SIZE) != getIdx) {
-        can.read(mailbox[putIdx]);
-        // check to see if id matches our accepted list
-        if (checkID(mailbox[putIdx].id)) {
-            // if we have a valid message we move to the next mailbox slot
-            putIdx = (putIdx + 1) % CAN_BUS_SIZE;
+        CANMessage msg;
+        if (can.read(msg)) {
+            /* Check to see if id matches our accepted list. */
+            if (checkID(msg.id)) {
+                /* If we have a valid message, add to the mailbox and move on. */
+                mailbox[putIdx] = msg;
+                putIdx = (putIdx + 1) % CAN_BUS_SIZE;
+            }
         }
     }
-    bufferLock = false;
+
+    mailboxLock = false;
 }
 
-bool CANDevice::checkID(const int id) {
-    switch (id) {
-        case MPPT_CAN_ID:
-            return true;
-        default:
-            return false;
+bool CANDevice::checkID(const int id) const {
+    /* Loop through the CAN ID whitelist and see if we have a whitelisted ID. */
+    for (uint32_t i = 0; i < whitelistLen; i++) {
+        if (whitelist[i] == id) return true;
     }
+    return false;
 }
--- src/CANDevice/CANDevice.h
+++ src/CANDevice/CANDevice.h
@@ -5,9 +5,9 @@
  * Author: Matthew Yu
  * Organization: UT Solar Vehicles Team
  * Created on: September 12th, 2020
- * Last Modified: 10/07/20
+ * Last Modified: 04/10/21
  * 
- * File Discription: This file manages the CAN class, abstracting away
+ * File Description: This file manages the CAN class, abstracting away
  * implementation logic to send and receive messages via the CAN lines and
  * providing an easy interface.
  */
@@ -15,11 +15,13 @@
 #include "mbed.h"
 #include <chrono>
 #include <string.h>
+#include <Definitions/Definitions.hpp>
 
 
-#define MPPT_CAN_ID 100
+#define CAN_ID_WHITELIST_LENGTH 20
 #define CAN_BUS_SIZE 200
 #define CAN_BUS_BAUD 25000
+#define INVALID_CAN_ID_DEFAULT 0x7FF /* This may be allocated to someone. Make sure it isn't. */
 
 
 /**
@@ -40,25 +42,61 @@ class CANDevice {
         CANDevice(const PinName pinRx, const PinName pinTx);
 
         /**
-         * Grabs a CAN message from the internal buffer, if any.
+         * Filters what messages can be added to the mailbox.
          * 
-         * @return Pointer to a CAN Message as a char array. 
-         * @note Must be freed.
+         * @param[in] ids A pointer to a list of IDs to capture.
+         * @param[in] len The number of IDs in the list.
+         * @return Whether the list was applied correctly. The applicable return
+         *         codes are ERROR (0) and OK (1).
+         * @note An error event can occur when len > CAN_ID_WHITELIST_LENGTH.
+         *       Subsequent calls of addFilter overwrites the previously set filter.
+         *       Call this prior to calling start() or after calling stop() for
+         *       best results.
          */
-        char* getMessage();
+        result_t addFilter(uint32_t *ids, uint32_t len);
 
         /**
-         * Starts interrupt execution of the private handler function given the 
+         * Sends a message over can.
+         * 
+         * @param[in] id The CAN ID assigned to the message.
+         * @param[in] message The CAN message to send.
+         * @param[in] len The number of bytes of the message.
+         * @return Whether a message was extracted from the mailbox. The
+         *         applicable return codes are ERROR (0) and OK (1).
+         */
+        result_t sendMessage(uint32_t id, char *message, uint8_t len);
+
+        /**
+         * Grabs a CAN message from the internal buffer, if any. The message is
+         * loaded into a pointer and then subsequently deleted from the internal
+         * buffer. Stalls if the CAN is currently handling a message.
+         * 
+         * @param[out] message    A pointer to an array to populate with the
+         *                        message.
+         * @param[in] messageSize How large the buffer is. Any output value
+         *                        larger than this is truncated.
+         * @return Whether a message was extracted from the mailbox. The
+         *         applicable return codes are OK (1) and NO_MESSAGE (2).
+         */
+        result_t getMessage(char *message, int messageSize);
+
+        /**
+         * Starts interrupt execution of the private handler function given the  
          * interval.
          * 
-         * @param[in] interval Time, in microseconds, between each function call.
+         * @param[in] interval Time, in microseconds, between each function
+         *                     call.
          */
         void start(const int interval);
         
         /**
-         * Stops interrupt execution of the private handler function given the interval.
+         * Stops interrupt execution of the private handler function given the
+         * interval. 
          */
         void stop();
+
+        /** Clears out the mailbox. */
+        void clear();
 
     private:
         /**
@@ -67,23 +105,36 @@ class CANDevice {
         void handler();
 
         /**
-         * Checks the ID against a list of CAN ids. If it matches we return success.
+         * Checks the ID against a list of CAN ids. If it matches we return
+         * success. 
          * 
          * @return True if ID matches our list, False otherwise.
          */
-        bool checkID(const int id); 
+        bool checkID(const int id) const; 
 
     private:
+
+        /** The CAN device used for sending and receiving messages. */
         CAN can;
-        CANMessage msg;
+
+        /** 
+         * The mailbox for receiving messages. They can be picked up
+         * asynchronously. This is a circular FIFO.
+         */ 
         CANMessage mailbox[CAN_BUS_SIZE];
-        Ticker tick;
 
         /** Lock for the mailbox. I hope you have a key. */
-        bool bufferLock;
+        bool mailboxLock;
 
         /** Indices for traversing the mailbox. */
         int getIdx;
         int putIdx;
+
+        /** Ticker used to capture messages. */
+        Ticker tick;
+
+        /** Whitelist and number of valid IDs in the whitelist. */
+        uint32_t whitelistLen;
+        uint32_t whitelist[CAN_ID_WHITELIST_LENGTH];
 };
 
