e249b9fecf92e54296fbb4855339863643bb4401
Matthew Yu <Matthewjkyu@gmail.com>
2021-06-25 20:00:48 -0500

    Added descriptions to errors.

--- src/Error/Errors.h
+++ src/Error/Errors.h
@@ -9,10 +9,11 @@
  */
 
 /* Standard Error Codes 0 - 255 (2^8). */
-#define ERR_NONE                0x00
-#define ERR_UNKNOWN             0x01
-#define ERR_BAD_STATE           0x02
-#define ERR_INVALID_CAN_ID      0x20
-#define ERR_INVALID_CAN_DATA    0x21
+#define ERR_NONE                0x00 /* No error has occurred. */
+#define ERR_UNKNOWN             0x01 /* Unknown/Undefined error. */
+#define ERR_BAD_STATE           0x02 /* Program defined state is not expected. */
+#define ERR_INVALID_CAN_ID      0x20 /* CAN ID was never allocated by user. */
+#define ERR_INVALID_CAN_DATA    0x21 /* CAN DATA is in a bad format. */
+#define ERR_UNEXPECTED_CAN_ID   0x22 /* CAN ID was not expected in context. */
 
 /* Extended Error Codes 256 - 65636 (2^16). */
